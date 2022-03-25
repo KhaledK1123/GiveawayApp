355146c4b81d4df968b8bd6538a405d24a5f0704
tylerporter30 <tylerporter30@gmail.com>
2022-03-24 20:41:17 -0500

    Commit 1, made a login screen, forget password, and create account page.

--- app/src/main/java/com/example/giveawayapp/MainActivity.kt
+++ app/src/main/java/com/example/giveawayapp/MainActivity.kt
@@ -3,13 +3,10 @@ package com.example.giveawayapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.giveawayapp.view.ui.theme.GiveawayAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +14,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GiveawayAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Greeting("Android")
+
+                    LoginText(displayText = "New To You")
+
+                    Login()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GiveawayAppTheme {
-        Greeting("Android")
     }
 }
