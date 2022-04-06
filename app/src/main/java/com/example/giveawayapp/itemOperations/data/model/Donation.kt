7116c25e858f2f00e1aa21ac891c74241b49f84b
rnavarro97 <richard.navarro.1618@gmail.com>
2022-04-06 05:10:02 -0400

    DonoVM and below complete

--- app/src/main/java/com/example/giveawayapp/itemOperations/data/model/Donation.kt
+++ app/src/main/java/com/example/giveawayapp/itemOperations/data/model/Donation.kt
@@ -2,16 +2,16 @@ package com.example.giveawayapp.itemOperations.data.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Donation (
+data class Donation(
 
     @SerializedName("id")
-    val id: Int,
+    override val id: Int,
+
+    @SerializedName("publisher")
+    override val publisher: String,
 
     @SerializedName("donation")
     val donationName: String,
-
-    @SerializedName("donor_username")
-    val donor_username: String,
 
     @SerializedName("location")
     val location: String,
@@ -23,12 +23,8 @@ data class Donation (
     val description: String,
 
     @SerializedName("tags")
-    val tags: Array<String>,
+    val tags: List<String>,
 
     @SerializedName("images")
-    val images: Array<String>
-        )  {
-
-
-
-}
+    val images: List<String>
+    ) : Listable()
---
+++ app/src/main/java/com/example/giveawayapp/itemOperations/data/model/Listable.kt
@@ -0,0 +1,6 @@
+package com.example.giveawayapp.itemOperations.data.model
+
+abstract class Listable {
+    abstract val id: Int
+    abstract val publisher: String
+}
